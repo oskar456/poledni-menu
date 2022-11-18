bd0ff2496e05ede07f617707deb28e319e000061
Jiri Raz <raz@cesnet.cz>
2022-11-18 13:21:38 +0100

    Changes in blox extactor

--- poledni_menu/extractors/blox.py
+++ poledni_menu/extractors/blox.py
@@ -5,7 +5,7 @@ from ..utils import parsed_html_doc
 
 
 def get_name():
-    return "Blox"
+    return "Restaurant & Café BLOX"
 
 
 def get_url():
@@ -20,14 +20,12 @@ def get_menu():
         '//*[@id="page_obedy"]//*[starts-with(text(), "{}")]'
         '/ancestor::tr/following-sibling::tr'.format(dayname),
     )
-    for meal in rows:
+    for meal in rows[0:8]:
         cols = meal.findall("td")
         if len(cols) == 4:
             num, name, alergens, price = cols
         else:
             continue
-        if num.text_content().strip() == "":
-            return
         if (name is not None) and (price is not None):
             mealname = name.text_content().strip()
             if mealname:
