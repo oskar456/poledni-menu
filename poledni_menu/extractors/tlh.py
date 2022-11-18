78f7adec2366748e69b60d9661d831e6012a4251
Jiri Raz <raz@cesnet.cz>
2022-11-18 13:23:22 +0100

    Add telehouse canteen extractor

---
+++ poledni_menu/extractors/tlh.py
@@ -0,0 +1,32 @@
+import datetime
+import locale
+
+from ..utils import parsed_html_doc
+
+
+def get_name():
+    return "Telehouse Canteen"
+
+
+def get_url():
+    return "https://telehouse-canteen.cz/#!/page_obedy"
+
+
+def get_menu():
+    doc = parsed_html_doc(get_url())
+    locale.setlocale(locale.LC_TIME, 'cs_CZ.UTF8')
+    dayname = datetime.date.today().strftime("%A")
+    rows = doc.xpath(
+        '//*[@id="page_obedy"]//*[starts-with(text(), "{}")]'
+        '/ancestor::tr/following-sibling::tr'.format(dayname),
+    )
+    for meal in rows[0:6]:
+        cols = meal.findall('td')
+        if len(cols) == 3:
+            name, alergens, price = cols
+        else:
+            continue
+        if (name is not None) and (price is not None):
+            mealname = name.text_content().strip()
+            if mealname:
+                yield (mealname, price.text_content().strip())
